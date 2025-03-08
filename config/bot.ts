e9ce22779b9cc1dd5cb7f6e225a24fa374238c53
akumaru <isakulovdev@gmail.com>
2025-03-08 12:22:51 +0500

    feat: simple debugger

--- config/bot.ts
+++ config/bot.ts
@@ -1,5 +1,17 @@
-import { Bot } from "../deps.ts";
+import { Bot, Context, NextFunction } from "../deps.ts";
 import "https://deno.land/std@0.201.0/dotenv/load.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") as string);
 export const instance = await bot.api.getMe();
+
+
+// Debugger
+bot.use(async (ctx: Context, next: NextFunction) => {
+    console.log(
+      `%c[INFO]`,
+      "color: green",
+      ctx.update,
+    );
+  
+    await next();
+  });
