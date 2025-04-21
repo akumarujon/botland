cafa6ac00a86d9a9fdf80a89d16ec5d989acf1f3
akumarujon <isakulovdev@gmail.com>
2025-04-21 23:06:25 +0500

    feat: simple error handler

--- config/bot.ts
+++ config/bot.ts
@@ -1,17 +1,30 @@
-import { Bot, Context, NextFunction } from "../deps.ts";
+import { Bot, Context, GrammyError, HttpError, NextFunction } from "../deps.ts";
 import "https://deno.land/std@0.201.0/dotenv/load.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") as string);
 export const instance = await bot.api.getMe();
 
-
 // Debugger
 bot.use(async (ctx: Context, next: NextFunction) => {
-    console.log(
-      `%c[INFO]`,
-      "color: green",
-      ctx.update,
-    );
-  
-    await next();
-  });
+  console.log(
+    `%c[INFO]`,
+    "color: green",
+    ctx.update,
+  );
+
+  await next();
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error as Error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
